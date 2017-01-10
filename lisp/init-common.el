a15d098b4af9988f69a503c7913e035da3f94e91
chentingliang <tingliang1024@gmail.com>
2017-01-10 17:22:43 +0800

    add snippet

--- lisp/init-common.el
+++ lisp/init-common.el
@@ -3,15 +3,15 @@
 
 ((lambda ()
    (global-linum-mode t)
-   (global-hl-line-mode 1)              ; highlight current mode
+   (global-hl-line-mode t)              ; highlight current mode
    (setq nyan-mode t)
    (column-number-mode t)
    (setq select-enable-clipboard t)
    (show-paren-mode t)
    (set-face-background 'show-paren-match "yellow")
    ;; (auto-image-file-mode t)
-   (setq-default display-time-format "%Y-%m-%d %H:%M %A") ; year-month-day hh:mm week
-   (display-time)
+   ;; (setq-default display-time-format "%Y-%m-%d %H:%M %A") ; year-month-day hh:mm week
+   ;; (display-time)
    (electric-pair-mode t)
    ;; (setq molokai-theme-kit t)
    ;; (load-theme 'molokai t)	 ; theme
@@ -26,7 +26,7 @@
    (electric-pair-mode 1)
    (put 'erase-buffer 'disabled nil)
    (recentf-mode 1)
-   (setq-default recentf-max-menu-items 25)
+   (setq-default recentf-max-menu-items 30)
    ;; encode
    (prefer-coding-system 'utf-8)
    (set-default-coding-systems 'utf-8)
